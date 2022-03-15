41cd78d969d121a627ebb8f896926a429818059c
Estelle <estelle.wu117@gmail.com>
2022-03-15 21:38:38 +1300

    add css

--- src/components/Length.js
+++ src/components/Length.js
@@ -1,32 +1,40 @@
 import React from 'react'
 import {Data1, Data2} from './LengthData'
 import { useState } from 'react'
+import './Length.css'
 
 const Length = () => {
     const [data, SetData]=useState(null)
     const [print, SetPrint]=useState(false)
 
     return (
-        <div id="App">
-            <div className="select-container">
-                <select>
-                    {Data1.map((option) => (
-                    <option value={option.value}>{option.label}</option>
-                    ))}
-                </select>
-                <input type="number" />
+        <div className='length-container'>
+            <div className='length'>
+                <div className='length-wrapper'>
+                    <div className="select-container">
+                        <select className='selection'>
+                            {Data1.map((option) => (
+                            <option value={option.value}>{option.label}</option>
+                            ))}
+                        </select>
+                        <input type="number" />
+                    </div>
+                    
+                    <div className="select-container">
+                        <select className='selection'>
+                            {Data2.map((option) => (
+                            <option value={option.value}>{option.label}</option>
+                            ))}
+                        </select>
+                        <input type="number" />
+                    </div>
+                </div>
+                
+                {/* button */}
+                <div className='select-container'>
+                    <button  className='convert-btn'>Convert</button>
+                </div>
             </div>
-            
-            <div className="select-container">
-                <select>
-                    {Data2.map((option) => (
-                    <option value={option.value}>{option.label}</option>
-                    ))}
-                </select>
-                <input type="number" />
-            </div>
-            {/* button */}
-            <button  >Convert</button>
       </div>
     )
 }
