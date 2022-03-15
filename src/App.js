9faa9ef7d71f9adac386479095e6450ad74afbc1
Estelle <estelle.wu117@gmail.com>
2022-03-15 20:04:13 +1300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes,Switch } from 'react-router-dom';
-import Home from './components/Home';
 import Celsius from './components/Celsius';
 import Hour from './components/Hour';
 import Length from './components/Length';
 import Bytes from './components/Bytes';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />}/> 
         <Route path="/celsius" element={<Celsius />}/>
         <Route path="/hour" element={<Hour />}/>
         <Route path="/length" element={<Length />}/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { FaHome } from "react-icons/fa";
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    {/* logo + icon */}
+                    <Link to='/' className='navbar-logo' >
+                        <FaHome />
+                    </Link>
+                   
+                    <ul className='nav-menu'>
+                        <li className='nav-item'>
+                            <Link to='/celsius' className='nav-links' >
+                                Celsius
+                            </Link>
+                        </li>
+                        <li className='nav-item' >
+                            <Link
+                                to='/hour'
+                                className='nav-links'
+                            >
+                                Hour 
+                            </Link>
+                            {/* {dropdown && <Dropdown />} */}
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/length'
+                                className='nav-links'
+                            >
+                                Length
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/bytes'
+                                className='nav-links'
+                            >
+                                Bytes
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
