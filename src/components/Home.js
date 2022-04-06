84953a4257b8830329faeb6e5534e07b07899cec
Estelle <estelle.wu117@gmail.com>
2022-04-07 11:27:20 +1200

    change home page name

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ const Home = () => {
     return (
         <div className='home'>
             <div className='home-container'>
-                <h1>MAGIQ Software</h1>
-                <h2>Graduate Technical Test</h2> 
-                <h3>- Part 2 -</h3>  
+                <h1>Converter</h1>
                 <h3>- Framework: Reactjs -</h3>  
             </div>
         </div>
